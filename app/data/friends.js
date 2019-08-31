242602b61470c0c0da880871d922fe051fb16363
Duane Stafford <duanestafford@Duanes-MBP.fios-router.home>
2019-08-31 14:19:35 -0400

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -18,7 +18,46 @@ var friendArray = [
             1
         ]
       
-    }
+    },
+
+    {
+      firstName: "David",
+      lastName: "Hasselhoff",
+      photoURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Hoff_3.jpg/800px-Hoff_3.jpg",
+      scores: [
+          3,
+          3,
+          3,
+          3,
+          5,
+          3,
+          3,
+          3,
+          3,
+          5
+      ]
+    
+  },
+  {
+    firstName: "Dwayne",
+    lastName: "Johnson",
+    photoURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Dwayne_Johnson_2%2C_2013.jpg/800px-Dwayne_Johnson_2%2C_2013.jpg",
+    scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+  
+},
+
+
   ];
 
 // Note how we export the array. This makes it accessible to other files using require.
